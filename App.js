170002d97b36df1c7769d048fba0b0c6c3f1b0d6
vk13194 <vijaym13194@gmail.com>
2019-06-20 05:35:56 -0700

    crud app with hooks

---
+++ App.js
@@ -0,0 +1,113 @@
+import React from 'react';
+//import React, {component} from 'react';
+import './App.css';
+//import ReactDOM from 'react-dom';
+//import Greet from './components/Greet';
+//import Form from './components/Form';
+//import {useState} from 'react';
+import Count from './components/Count';
+
+
+
+/*function App() {
+  return (
+    <div className="App">
+      <h1>hello react</h1>
+    </div>
+  );
+}*/
+class App extends React.Component {
+  render() {
+     return (
+        <div >
+           <Count />
+        </div>
+     );
+  }
+}
+/*function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count- 1)}>-</button>
+    </>
+  );
+}*/
+/*import  { useState, Fragment } from 'react'
+import AddUserForm from './components/AddUserForm'
+import EditUserForm from './components/EditUserForm'
+import UserTable from './components/UserTable'
+
+const App = () => {
+	// Data
+	const usersData = [
+		{ id: 1, name: 'ram', username: 'vijaym98' },
+		{ id: 2, name: 'ajay', username: 'vijaym99' },
+		{ id: 3, name: 'tom', username: 'vijaym95' },
+	]
+
+	const initialFormState = { id: null, name: '', username: '' }
+
+	// Setting state
+	const [ users, setUsers ] = useState(usersData)
+	const [ currentUser, setCurrentUser ] = useState(initialFormState)
+	const [ editing, setEditing ] = useState(false)
+
+	// CRUD operations
+	const addUser = user => {
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
+
+	const deleteUser = id => {
+		setEditing(false)
+
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false)
+
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editRow = user => {
+		setEditing(true)
+
+		setCurrentUser({ id: user.id, name: user.name, username: user.username })
+	}
+
+	return (
+		<div className="container">
+			<h1>CRUD App with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{editing ? (
+						<Fragment>
+							<h2>Edit user</h2>
+							<EditUserForm
+								editing={editing}
+								setEditing={setEditing}
+								currentUser={currentUser}
+								updateUser={updateUser}
+							/>
+						</Fragment>
+					) : (
+						<Fragment>
+							<h2>Add user</h2>
+							<AddUserForm addUser={addUser} />
+						</Fragment>
+					)}
+				</div>
+				<div className="flex-large">
+					<h2>View users</h2>
+					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+				</div>
+			</div>
+		</div>
+	)
+}*/
+export default App;
